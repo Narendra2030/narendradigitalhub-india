267d1be6aecd372ab4421595d47c8c7ae597fcc1
Narendra2030 <knk07744@gmail.com>
2026-06-12 18:23:55 +0530

    Update App.js

--- App.js
+++ App.js
@@ -6,26 +6,27 @@ getAuth,
 createUserWithEmailAndPassword,
 signInWithEmailAndPassword
 }
-from
-"https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const firebaseConfig = {
 
-apiKey: "PASTE_API_KEY",
+apiKey: "AIzaSyCIjsySkhok2u7qzFgMHqK9wfULrKgsYvY",
 
-authDomain: "PASTE_AUTH_DOMAIN",
+authDomain: "nk-digital-hub.firebaseapp.com",
 
-projectId: "PASTE_PROJECT_ID",
+projectId: "nk-digital-hub",
 
-appId: "PASTE_APP_ID"
+storageBucket: "nk-digital-hub.firebasestorage.app",
+
+messagingSenderId: "313625666009",
+
+appId: "1:313625666009:web:78c7cfdda635c51034b9c8"
 
 };
 
-const app =
-initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
-const auth =
-getAuth(app);
+const auth = getAuth(app);
 
 window.signup = function(){
 
@@ -35,6 +36,58 @@ document.getElementById("email").value;
 const password =
 document.getElementById("password").value;
 
+createUserWithEmailAndPassword(
+auth,
+email,
+password
+)
+
+.then(()=>{
+
+document.getElementById("msg").innerHTML =
+"Account Created Successfully";
+
+window.location = "login.html";
+
+})
+
+.catch((error)=>{
+
+document.getElementById("msg").innerHTML =
+error.message;
+
+});
+
+};
+
+window.login = function(){
+
+const email =
+document.getElementById("email").value;
+
+const password =
+document.getElementById("password").value;
+
+signInWithEmailAndPassword(
+auth,
+email,
+password
+)
+
+.then(()=>{
+
+window.location = "dashboard.html";
+
+})
+
+.catch((error)=>{
+
+document.getElementById("msg").innerHTML =
+error.message;
+
+});
+
+};
 createUserWithEmailAndPassword(
 auth,
 email,
